cb4296469192ff9dbc9e7bc60533da1827ee5c97
Chayanin <arm.crazyplay@gmail.com>
2023-08-03 11:26:39 +0700

    first commit

---
+++ src/components/Detail.jsx
@@ -0,0 +1,13 @@
+import { ThemeContext } from "@/contexts/ThemeContext";
+import { useContext } from "react";
+
+export const Detail = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <p className="text-center" style={{ color: theme.fgColor }}>
+      Display activated theme message here (EN or TH)
+      {/* tip: Get information from "theme" variable to determine 
+      which theme is currently selected */}
+    </p>
+  );
+};
