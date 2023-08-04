f761aa5a7d81633f00b4463935847f973b10b9c5
weerr12 <jokerwwe0542@gmail.com>
2023-08-04 11:08:39 +0700

    ko

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function HomePage() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+      <Footer year="2023" fullName="Werasak Mayer" studentId="650610808" />
     </div>
   );
 }
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,16 @@
+import { LangContext } from "@/contexts/LangContext";
 import { ThemeContext } from "@/contexts/ThemeContext";
 import { useContext } from "react";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
       Display activated theme message here (EN or TH)
+      {theme.name === "dark"
+        ? lang.detail.darkActivated
+        : lang.detail.lightActivated}
       {/* tip: Get information from "theme" variable to determine 
       which theme is currently selected */}
     </p>
